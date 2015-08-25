b9ab2ca623fcf2d8f66a137789615bb5cbc1d48e
Frank Terbeck <ft@bewatermyfriend.org>
2015-08-26 00:02:09 +0200

    render: Allow LaTeX renderer to insert summaries
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/work/render.scm
+++ scheme/work/render.scm
@@ -7,12 +7,27 @@
 (define* (render-calendar #:key
                           (data '())
                           (type 'pretty-print)
-                          (detailed? #f))
+                          (detailed? #f)
+                          (with-summary? #f))
   (case type
     ((pretty-print)
      (pretty-print data #:width 150 #:max-expr-width 150))
     ((latex)
-     (let ((struct '(header days summary)))
+     (let ((struct '(header days summary))
+           (*header* (line-style #:colour 'gray
+                                 #:intensity 0.8
+                                 #:size 'scriptsize
+                                 #:text-colour 'rgb
+                                 #:text-hue '(1 1 1)))
+           (*days-a* (if detailed?
+                         (line-style #:colour 'gray
+                                     #:intensity 0.25)
+                         (line-style)))
+           (*days-b* (line-style #:colour 'gray
+                                 #:intensity 0.25))
+           (*tasks* (line-style #:size 'tiny))
+           (*summary* (line-style #:colour 'gray
+                                  #:intensity 0.1)))
        (latex-document
         #:content
         (lambda ()
@@ -21,22 +36,14 @@
                                              (cons 'tasks struct)
                                              struct)
                       #:alternating-shade (not detailed?)
-                      #:styles (pretty-styles #:header
-                                              (line-style #:colour 'gray
-                                                          #:intensity 0.8
-                                                          #:size 'scriptsize
-                                                          #:text-colour 'rgb
-                                                          #:text-hue '(1 1 1))
-                                              #:days-a
-                                              (if detailed?
-                                                  (line-style #:colour 'gray
-                                                              #:intensity 0.25)
-                                                  (line-style))
-                                              #:days-b
-                                              (line-style #:colour 'gray
-                                                          #:intensity 0.25)
-                                              #:tasks (line-style #:size 'tiny)
-                                              #:summary
-                                              (line-style #:colour 'gray
-                                                          #:intensity 0.1)))))))
+                      #:styles (pretty-styles #:header *header*
+                                              #:days-a *days-a*
+                                              #:days-b *days-b*
+                                              #:tasks *tasks*
+                                              #:summary *summary*))
+          (when with-summary?
+            (summary-for data
+                         #:styles (pretty-summary #:header *header*
+                                                  #:balance *days-b*
+                                                  #:off-days *summary*)))))))
     (else (throw 'unknown-renderer type data))))
