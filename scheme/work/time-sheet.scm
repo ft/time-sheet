af8229a2aa872412dfe98c09c5dcf4a6e2f831cf
Frank Terbeck <ft@bewatermyfriend.org>
2015-08-27 23:54:00 +0200

    time-sheet: Stricter merging and checks
    ‘merge-time-sheets’ does not allow duplicate ‘id’ keys in the data base
    anymore. ‘ensure-user’ may be used to make sure the data base only
    contains entries by one single user.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/work/time-sheet.scm
+++ scheme/work/time-sheet.scm
@@ -1,7 +1,32 @@
 (define-module (work time-sheet)
-  #:export (merge-time-sheets))
+  #:export (ensure-user
+            merge-time-sheets))
 
-;; TODO: This is waaaay to simple; merging should at least check, if entries
-;;       exist more than once!
+(define (ensure-user user data)
+  (map (lambda (x)
+         (let ((this-user (assq-ref x 'person)))
+           (cond ((not this-user)
+                  (throw 'missing-user-name x))
+                 ((not (string=? user this-user))
+                  (throw 'wrong-user user x))
+                 (else x))))
+       data))
+
+(define (id< a b)
+  (< (assq-ref a 'id)
+     (assq-ref b 'id)))
+
 (define (merge-time-sheets lst)
-  (apply append lst))
+  (let ((sorted (sort (apply append lst) id<)))
+    (let loop ((rest sorted)
+               (prev-entry #f))
+      (cond ((null? rest) sorted)
+            ((not prev-entry)
+             (loop (cdr rest)
+                   (car rest)))
+            (else (let* ((this-entry (car rest))
+                         (this (assq-ref this-entry 'id))
+                         (prev (assq-ref prev-entry 'id)))
+                    (if (= this prev)
+                        (throw 'duplicate-id prev-entry this-entry)
+                        (loop (cdr rest) this-entry))))))))
