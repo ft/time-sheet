e258c619a9d29634e37825014f5c4ff885c71a6d
Frank Terbeck <ft@bewatermyfriend.org>
2015-09-13 18:11:30 +0200

    files: Add ‘match-files’ utility
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/time-sheet/utils/file.scm
+++ scheme/time-sheet/utils/file.scm
@@ -4,8 +4,35 @@
 ;; Terms for redistribution and use can be found in LICENCE.
 
 (define-module (time-sheet utils file)
-  #:export (read-file))
+  #:use-module (ice-9 ftw)
+  #:use-module (ice-9 optargs)
+  #:export (match-files
+            read-file))
 
 (define (read-file file)
   (with-input-from-file file
     (lambda () (read (current-input-port)))))
+
+(define (scandir* dir rx?)
+  (or (scandir dir rx? (lambda (x y) #t))
+      '()))
+
+(define (with-dir d f)
+  (string-concatenate (list d file-name-separator-string f)))
+
+(define* (match-files #:key
+                      (directory ".")
+                      (directories '())
+                      (regexp "."))
+  (define rx (make-regexp regexp regexp/extended))
+  (define (regex-matches? x) (regexp-exec rx x))
+  (let loop ((rest (if (null? directories)
+                       (list directory)
+                       directories))
+             (acc '()))
+    (cond ((null? rest) acc)
+          (else (loop (cdr rest)
+                      (let ((dir (car rest)))
+                        (append acc (map (lambda (x)
+                                           (with-dir dir x))
+                                         (scandir* dir regex-matches?)))))))))
