b1f9957ada37a71aca7603cdc57a62c674cd6d40
Frank Terbeck <ft@bewatermyfriend.org>
2015-09-18 20:15:11 +0200

    script: Add support for configuring language
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/time-sheet/script.scm
+++ scheme/time-sheet/script.scm
@@ -12,9 +12,16 @@
 (define (valid-mode? mode)
   (member mode *modes*))
 
+(define *languages* '("english" "german"))
+
+(define (valid-language? lang)
+  (member lang *languages*))
+
 (define *optspec* `((help (single-char #\h) (value #f))
                     (mode (single-char #\m) (value #t)
-                          (predicate ,valid-mode?))))
+                          (predicate ,valid-mode?))
+                    (lang (single-char #\l) (value #t)
+                          (predicate ,valid-language?))))
 
 (define (bool-opt? opts opt)
   (option-ref opts opt #f))
@@ -23,7 +30,9 @@
   (format #t "usage: ~a [OPTION(s)...]~%~%" cmd)
   (format #t "  --help, -h                   Display this help text.~%")
   (format #t "  --mode <MODE>, -m <MODE>     Specify a time-sheet mode.~%")
+  (format #t "  --lang <LANG>, -l <LANG>     Specify language for time-sheet.~%")
   (format #t "~%Available modes: compact, detailed, complete.~%")
+  (format #t "Available languages: german, english.~%")
   (quit 0))
 
 (define (mode->detailed-predicate mode)
@@ -35,7 +44,9 @@
 (define (script-options cmdline)
   (let* ((cmd (car cmdline))
          (options (getopt-long cmdline *optspec*))
-         (mode (string->symbol (option-ref options 'mode "compact"))))
+         (mode (string->symbol (option-ref options 'mode "compact")))
+         (lang (string->symbol (option-ref options 'lang "english"))))
     (cond ((bool-opt? options 'help)
            (help-text cmd))
-          (else (list (cons 'detailed? (mode->detailed-predicate mode)))))))
+          (else (list (cons 'detailed? (mode->detailed-predicate mode))
+                      (cons 'language lang))))))
