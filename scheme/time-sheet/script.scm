82d9132e0a1d23f8fb620e70aab3ffcb652ef166
Frank Terbeck <ft@bewatermyfriend.org>
2015-09-18 20:21:33 +0200

    script: Add support for configuring output type
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- scheme/time-sheet/script.scm
+++ scheme/time-sheet/script.scm
@@ -17,11 +17,18 @@
 (define (valid-language? lang)
   (member lang *languages*))
 
+(define *types* '("latex" "pp"))
+
+(define (valid-type? type)
+  (member type *types*))
+
 (define *optspec* `((help (single-char #\h) (value #f))
                     (mode (single-char #\m) (value #t)
                           (predicate ,valid-mode?))
                     (lang (single-char #\l) (value #t)
-                          (predicate ,valid-language?))))
+                          (predicate ,valid-language?))
+                    (type (single-char #\t) (value #t)
+                          (predicate ,valid-type?))))
 
 (define (bool-opt? opts opt)
   (option-ref opts opt #f))
@@ -31,8 +38,10 @@
   (format #t "  --help, -h                   Display this help text.~%")
   (format #t "  --mode <MODE>, -m <MODE>     Specify a time-sheet mode.~%")
   (format #t "  --lang <LANG>, -l <LANG>     Specify language for time-sheet.~%")
+  (format #t "  --type <TYPE>, -t <TYPE>     Specify output type.~%")
   (format #t "~%Available modes: compact, detailed, complete.~%")
   (format #t "Available languages: german, english.~%")
+  (format #t "Available output types: latex, pp.~%")
   (quit 0))
 
 (define (mode->detailed-predicate mode)
@@ -41,12 +50,19 @@
               (complete . extreme))
             mode))
 
+(define (type->type type)
+  (assq-ref '((latex . latex)
+              (pp . pretty-print))
+            type))
+
 (define (script-options cmdline)
   (let* ((cmd (car cmdline))
          (options (getopt-long cmdline *optspec*))
          (mode (string->symbol (option-ref options 'mode "compact")))
-         (lang (string->symbol (option-ref options 'lang "english"))))
+         (lang (string->symbol (option-ref options 'lang "english")))
+         (type (string->symbol (option-ref options 'type "latex"))))
     (cond ((bool-opt? options 'help)
            (help-text cmd))
           (else (list (cons 'detailed? (mode->detailed-predicate mode))
-                      (cons 'language lang))))))
+                      (cons 'language lang)
+                      (cons 'type (type->type type)))))))
